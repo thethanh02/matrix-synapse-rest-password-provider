45cda9ec8e8936957bef3ccdfac5cce1755cedd8
thanh.nt <thanh.nt@tinasoft.com.vn>
2024-10-31 11:15:54 +0700

    feat: missing modules setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="rest_auth_provider",
     version="0.0.1",
-    py_modules=['rest_auth_provider'],
+    py_modules=['rest_auth_provider', 'lao_id_provider'],
     description="Password Provider for Synapse fetching data from a REST endpoint",
     include_package_data=True,
     zip_safe=True,
